4e5b24c1b74f467602f58cdc7f72139585718407
William Pietri <william@scissor.com>
2017-12-28 10:50:15 -0800

    releasing 0.8.4

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ except FileNotFoundError:
 
 setup(
     name='sucks',
-    version='0.8.3',
+    version='0.8.4',
 
     description='a library for controlling certain robot vacuums',
     long_description=long_description,
