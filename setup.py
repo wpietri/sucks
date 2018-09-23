f2a683c026cb710dd6558050cc9e1c167d5579d9
William Pietri <william@scissor.com>
2018-09-22 17:50:20 -0700

    Bump version number to release bug fix.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ except FileNotFoundError:
 
 setup(
     name='sucks',
-    version='0.9.2',
+    version='0.9.3',
 
     description='a library for controlling certain robot vacuums',
     long_description=long_description,
